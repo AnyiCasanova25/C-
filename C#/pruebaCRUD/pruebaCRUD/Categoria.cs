23901619df95c4d05242ef821857566e992912c9
AnyiCasanova25 <146897637+AnyiCasanova25@users.noreply.github.com>
2024-07-26 12:30:50 -0500

    Trabajos C#

---
+++ C#/pruebaCRUD/pruebaCRUD/Categoria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebaCRUD
+{
+    public class Categoria
+    {
+        public string IdCategoria { get; set; }
+        public string nombreCategoria { get; set; }
+        public string descripcionCategoria { get; set; }
+
+        public Categoria(string idCategoria, string nombreCategoria, string descripcionCategoria)
+        {
+            IdCategoria = idCategoria;
+            this.nombreCategoria = nombreCategoria;
+            this.descripcionCategoria = descripcionCategoria;
+        }
+
+        public override string ToString() {
+            return $"Categoria: {nombreCategoria}, Descripcion: {descripcionCategoria}";
+        }
+    }
+}
---
+++ C#/pruebaCRUD/pruebaCRUD/Producto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebaCRUD
+{
+    public class Producto
+    {
+        public string IdProducto { get; set; }
+        public string nombreProducto { get; set; }
+        public string descripcionProducto { get; set; }
+        public decimal Precio { get; set; }
+
+        //relacion con la categoria
+        private Categoria Categoria { get; set; }
+
+        // Metodo Constructor
+        public Producto(string id, string nombre, string descripcion, decimal precio,Categoria categoria)
+        {
+            IdProducto = id;
+            nombreProducto = nombre;
+            descripcionProducto = descripcion;
+            Precio = precio;
+            Categoria = categoria;
+
+        }
+
+        public override string ToString()//Hace que devuelva nombre y descripcion
+        {
+            return $"Producto: {nombreProducto}, Descripcion: {descripcionProducto}";
+        }
+    }
+}
